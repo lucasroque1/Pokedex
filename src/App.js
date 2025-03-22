e51838616f9ba725d10c2fe531136b424f25ae2c
lucasroque1 <lucasrjacomine@gmail.com>
2025-03-22 16:02:00 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './style.css'
 
 function App() {
+  const [pokemon, setPokemon] = useState({});
+  const [nomePokemon, setNomePokemon] = useState('');
+
+
+  function loadAPI(nome = 'charizard') {
+    let url = `https://pokeapi.co/api/v2/pokemon/${nome.toLowerCase()}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(res => {
+        console.log(res)
+        setPokemon(res)
+      })
+      .catch(err => console.log(err));
+  }
+
+
+  useEffect(() => {
+
+    loadAPI();
+
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (nomePokemon.trim() !== '') {
+      loadAPI(nomePokemon);
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <strong> Pokedex </strong>
       </header>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o nome do Pokémon"
+          value={nomePokemon}
+          onChange={(e) => setNomePokemon(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+
+
+
+      <div>
+      {pokemon.sprites && (
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      )}
+        <div>Name: {pokemon.name}</div>
+        <div>N° {pokemon.id}</div>
+        <div>Peso: {pokemon.weight / 10}kg</div>
+        <div>Altura: {pokemon.height / 10}m</div>
+      </div>
     </div>
   );
 }
